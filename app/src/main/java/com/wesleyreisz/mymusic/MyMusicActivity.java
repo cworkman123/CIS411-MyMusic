11e3f56e3e44ead790512e2e98172b0cae7b84db
cworkman123 <cworkman@channingworkman.com>
2014-10-20 11:30:24 -0400

    Populate List View with Song Data

--- app/src/main/java/com/wesleyreisz/mymusic/MyMusicActivity.java
+++ app/src/main/java/com/wesleyreisz/mymusic/MyMusicActivity.java
@@ -4,14 +4,52 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.wesleyreisz.mymusic.model.Song;
+import com.wesleyreisz.mymusic.service.MockMusicService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MyMusicActivity extends Activity {
+
+    private ArrayAdapter<String> listAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_music);
+
+        //Initialize List View and Populate List<Song> Values via findAll() method
+        ListView listView = (ListView) findViewById(R.id.listview);
+        List<Song> songs = new MockMusicService().findAll();
+
+        ArrayList<String> songList = new ArrayList<String>();
+
+        for(Song song : songs ){
+            String songTitle = song.getSongTitle();
+            String artist = song.getArtistName();
+            String album = song.getAlbumTitle();
+            String date = song.getSongPublishedDate().toString();
+            String newLine = System.getProperty("line.separator");
+
+            String record = songTitle + newLine + "" + artist + newLine + "" + album + newLine +"" + date;
+
+            songList.add(record);
+
+
+        }
+
+        //Create ArrayAdapter using the songlist
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, songList);
+
+        //Set the ArrayAdapter as the ListView's adapter.
+        listView.setAdapter(listAdapter);
+
     }
 
 
